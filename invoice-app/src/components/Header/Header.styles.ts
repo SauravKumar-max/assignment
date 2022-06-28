659be93ef6ae9682fa86b60f479359bb6c1062be
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 22:57:42 +0530

    style: header component

---
+++ invoice-app/src/components/Header/Header.styles.ts
@@ -0,0 +1,16 @@
+export const styles = {
+    text: {
+        color: "text.primary",
+        fontSize: "2rem",
+        fontWeight: "bold"
+    },
+
+    smallText: {
+        color: "text.secondary",
+        fontSize: "0.8rem"
+    },
+
+    button: {
+        fontWeight: "bold"
+    }
+}
