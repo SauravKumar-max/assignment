78d218df515d43bb22459ee7394ef7a85582d6dd
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 22:59:20 +0530

    style: invoice item, card

---
+++ invoice-app/src/components/Invoice/Invoice.styles.ts
@@ -0,0 +1,64 @@
+export const styles = {
+    itemContainer: {
+        width: "91%",
+        backgroundColor: "#1e2139",
+        color: "text.primary",
+        height: "5rem",
+        padding: "0 1rem",
+        borderRadius: "0.4rem",
+        cursor: "pointer",
+        fontWeight: "bold",
+        margin: "1rem auto"
+    },
+
+    lightText:{
+        display: "flex",
+        color: 'text.secondary',
+        fontSize: "0.9rem"
+    },
+
+    boldText: {
+        color: "text.primary",
+        fontWeight: "bold",
+        fontSize: "0.9rem"
+    },
+
+    price: {
+        fontSize: "1.3rem",
+        fontWeight: "bold"
+    },
+
+    listContainer: {
+        display: "flex",
+        flexDirection: "column-reverse",
+        margin: "3rem 1rem"
+    },
+
+    cardContainer: {
+        width: "90%",
+        backgroundColor: "#1e2139",
+        color: "text.primary",
+        padding: "1rem",
+        borderRadius: "0.4rem",
+        margin: "1rem auto"
+    },
+
+    largeText: {
+        fontSize: "1.3rem",
+        fontWeight: "bold"
+    },
+
+    table: { 
+        marginTop: "2rem", 
+        border: "solid 1px #fff" 
+    },
+
+    link: {
+        color: "#fff",
+        fontWeight: "bold",
+        textDecoration: "none",
+        background: "#7c5dfa",
+        padding: "0.5rem 1rem",
+        borderRadius: "0.2rem"
+    }
+}
