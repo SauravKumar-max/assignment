695c2875e7b61e37260ef09c08dcdb71db721c75
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 23:00:33 +0530

    style: skeleton list, card

---
+++ invoice-app/src/components/Skeleton/Skeleton.styles.ts
@@ -0,0 +1,15 @@
+export const styles = {
+    skeletonItem: {
+        margin: "1rem auto",
+        width:"95%",
+        height: "5rem",
+        borderRadius: "0.4rem"
+    },
+
+    skeletonCard: {
+        margin: "1rem auto",   
+        width:"95%",
+        height: "30rem",
+        borderRadius: "0.4rem"
+    }
+}
