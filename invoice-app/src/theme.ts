16176b8f0b189bd2156e7768c8671ceab41d974a
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 22:55:40 +0530

    MUI theme

---
+++ invoice-app/src/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        background: {
+            default: "#141625"
+        },
+        primary: {
+            main: '#7c5dfa',
+        },
+        secondary: {
+            main: "#1e2139",
+        },
+        text: {
+            primary: "#fff",
+            secondary: "#dfe3fa",
+        }
+    },
+});
