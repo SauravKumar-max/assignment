48ab6e5c29451bff3f82ec293c5a4352a87c4e61
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 23:01:53 +0530

    style: add invoice dialog and form

---
+++ invoice-app/src/features/invoices/AddInvoice/AddInvoice.styles.ts
@@ -0,0 +1,40 @@
+export const styles = {
+    background: {
+        background: "#1e2139"
+    },
+
+    boldText: {
+        fontWeight: "bold"
+    },
+
+    textField: {
+        minWidth: "15rem",
+        margin: "0.5rem",
+        "& .MuiOutlinedInput-root": {
+            "& > fieldset": { borderColor: "#fff" },
+        },
+    },
+
+    fullTextField: {
+        margin: "0.5rem",
+        width: "93%",
+        "& .MuiOutlinedInput-root": {
+            "& > fieldset": { borderColor: "#fff" },
+        },
+    },
+
+    lineTextField: {
+        margin: "0.5rem 0.2rem",
+        "& .MuiOutlinedInput-root": {
+            "& > fieldset": { borderColor: "#fff" },
+        },
+    },
+
+    lineFullTextField: {
+        margin: "0.5rem 0.2rem",
+        width: "98%",
+        "& .MuiOutlinedInput-root": {
+            "& > fieldset": { borderColor: "#fff" },
+        },
+    }
+}
