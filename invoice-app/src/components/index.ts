007545267ddffc3809399a8cb8ff592a0487f81b
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 22:57:21 +0530

    exported files

---
+++ invoice-app/src/components/index.ts
@@ -0,0 +1,5 @@
+export { Header } from "./Header/Header";
+export { InvoiceListItem } from "./Invoice/ListItem";
+export { InvoiceList } from "./Invoice/List";
+export { InvoiceCard } from "./Invoice/Card";
+export { InvoiceSkeletonCard } from "./Skeleton/InvoiceSkeletonCard"
---
+++ invoice-app/src/page/index.ts
@@ -0,0 +1,2 @@
+export { Home } from "./Home";
+export { Invoice } from "./Invoice";
