ca9b950c051164e2d1f82a1cb0d9cc3781abeee7
Saurav Kumar <maxsaurav237@gmail.com>
2022-06-28 23:02:41 +0530

    feat: created Home pahe

---
+++ invoice-app/src/page/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header, InvoiceList } from "../components";
+
+export function Home() {
+  return (
+    <div>
+      <Header />
+      <InvoiceList />
+    </div>
+  );
+}
